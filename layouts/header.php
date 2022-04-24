72bbc159cf912971b8767c247c248e0c746f5e34
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2022-04-24 20:20:47 +0430

    change color

--- layouts/header.php
+++ layouts/header.php
@@ -8,11 +8,11 @@
     <!-- cdn content delivery network -->
     <style type="text/css">
         .brand {
-            background: #cbb09c !important;
+            background: #607d8b !important;
         }
 
         .brand-text {
-            color: #cbb09c !important;
+            color: #607d8b !important;
         }
 
         form {
